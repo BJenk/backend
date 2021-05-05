dfab7a116c771bbab923b5aa91ab2e8de758690b
Ben  Jenkins <bjenkins@automatedenergy.com>
2021-05-05 16:22:39 -0500

    first push

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5ea436eba39705e2377735aaccd8a013'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '11198b51-dbe2-4538-8c52-53942a2905be'
+};
